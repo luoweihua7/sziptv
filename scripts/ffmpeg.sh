7f4ef329118f2bbd0f73399698b08d694e2ad7cf
Larify <luowehua7@gmail.com>
2022-04-03 13:58:27 +0800

    feat: 深圳 IPTV 的组播地址

---
+++ scripts/ffmpeg.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+today=`date '+%Y-%m-%d'`
+snapshot_dir="./snapshot/$today"
+mkdir -p $snapshot_dir
+
+# 修改为自己的uproxy地址和端口
+uproxy_host="10.0.0.1"
+uproxy_port="10001"
+
+for i in {0..1}
+do
+  for j in {1..255}
+  do
+    now=`date '+%Y-%m-%d %H:%M:%S'`
+    echo "[$now] 239.77.$i.$j"
+    ffmpeg -i "http://$uproxy_host:$uproxy_port/rtp/239.77.$i.$j:5146/" -f image2 -vframes 3 -loglevel quiet $snapshot_dir/239_77_$i\_$j.jpeg
+  done
+done
+
+
