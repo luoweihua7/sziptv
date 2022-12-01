7d546a3d9fb6e2b7ec8dbe5de6dbd7deb5a4c272
Larify <luoweihua7@gmail.com>
2022-12-01 15:57:09 +0800

    添加超时时间，解决部分IP会卡住无法截图的问题

--- scripts/ffmpeg.sh
+++ scripts/ffmpeg.sh
@@ -4,7 +4,7 @@ today=`date '+%Y-%m-%d'`
 snapshot_dir="./snapshot/$today"
 mkdir -p $snapshot_dir
 
-# 修改为自己的uproxy地址和端口
+# 修改为自己的uproxy配置的地址和端口
 uproxy_host="10.0.0.1"
 uproxy_port="10001"
 
@@ -14,7 +14,8 @@ do
   do
     now=`date '+%Y-%m-%d %H:%M:%S'`
     echo "[$now] 239.77.$i.$j"
-    ffmpeg -i "http://$uproxy_host:$uproxy_port/rtp/239.77.$i.$j:5146/" -f image2 -vframes 3 -loglevel quiet $snapshot_dir/239_77_$i\_$j.jpeg
+    # kill ffmpeg after 15 seconds
+    timeout -k 10 15 ffmpeg -i "http://$uproxy_host:$uproxy_port/rtp/239.77.$i.$j:5146/" -f image2 -vframes 3 -loglevel quiet $snapshot_dir/239_77_$i\_$j.jpeg
   done
 done
 
