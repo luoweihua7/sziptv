b39f01f6b43e60ef9885ed93c1864e5d6bee6e33
Larify <luoweihua7@gmail.com>
2022-12-01 18:34:55 +0800

    feat: 添加新的网段扫描

--- scripts/ffmpeg.sh
+++ scripts/ffmpeg.sh
@@ -7,16 +7,39 @@ mkdir -p $snapshot_dir
 # 修改为自己的uproxy配置的地址和端口
 uproxy_host="10.0.0.1"
 uproxy_port="10001"
+uproxy_url="http://$uproxy_host:$uproxy_port/rtp"
 
-for i in {0..1}
+function screenshot() {
+  rtsp_ip=$1
+  now=`date '+%Y-%m-%d %H:%M:%S'`
+  echo "[$now] $rtsp_ip"
+  # kill ffmpeg after 15 seconds
+  timeout -k 10 15 ffmpeg -i "$uproxy_url/$rtsp_ip:5146/" -f image2 -vframes 3 -loglevel quiet $snapshot_dir/$rtsp_ip.jpeg
+}
+
+# 239.77 网段
+for i in 0 1 
 do
   for j in {1..255}
   do
-    now=`date '+%Y-%m-%d %H:%M:%S'`
-    echo "[$now] 239.77.$i.$j"
-    # kill ffmpeg after 15 seconds
-    timeout -k 10 15 ffmpeg -i "http://$uproxy_host:$uproxy_port/rtp/239.77.$i.$j:5146/" -f image2 -vframes 3 -loglevel quiet $snapshot_dir/239_77_$i\_$j.jpeg
+    rtsp_ip="239.77.$i.$j"
+    screenshot $rtsp_ip
   done
 done
 
+# 239.253.43 网段
+for i in {1..255}
+do
+  rtsp_ip="239.253.43.$i"
+  screenshot $rtsp_ip
+done
 
+# 239.0 网段
+for i in 0 10
+do
+  for j in {1..255}
+  do
+    rtsp_ip="239.0.$i.$j"
+    screenshot $rtsp_ip
+  done
+done
