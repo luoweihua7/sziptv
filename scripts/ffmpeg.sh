ca048a6aab508ea4c5a44c0a049f8bc689baa2d9
Larify <3157520+luoweihua7@users.noreply.github.com>
2023-01-03 20:23:38 +0800

    Update ffmpeg.sh

--- scripts/ffmpeg.sh
+++ scripts/ffmpeg.sh
@@ -11,10 +11,11 @@ uproxy_url="http://$uproxy_host:$uproxy_port/rtp"
 
 function screenshot() {
   rtsp_ip=$1
+  rtsp_url="$uproxy_url/$rtsp_ip:5146/"
   now=`date '+%Y-%m-%d %H:%M:%S'`
-  echo "[$now] $rtsp_ip"
+  echo "[$now] $rtsp_url"
   # 尝试截图，并自动结束长时间未能截图的进程，实现自动轮训下一个IP截图
-  ffmpeg -t 3 -i "$uproxy_url/$rtsp_ip:5146/" -f image2 -vframes 3 -loglevel quiet $snapshot_dir/$rtsp_ip.jpeg
+  ffmpeg -t 3 -i "$rtsp_url" -f image2 -vframes 15 $snapshot_dir/$rtsp_ip.jpeg
 }
 
 # 239.77 网段
